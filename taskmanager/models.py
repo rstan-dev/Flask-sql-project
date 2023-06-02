fd707a8102b51d4700dfbbf16993abe4f5c50546
Russ-S <rstanleym74@gmail.com>
2023-06-02 04:53:08 +0000

    add categories page and add_catgeory page, add button div, add form to submit new category data, create route for add_category

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -5,13 +5,11 @@ class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
-    tasks = db.relationship("Task", backref="category", cascade ="all, delete", lazy=True)
-
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return self.category_name
-
 
 
 class Task(db.Model):
