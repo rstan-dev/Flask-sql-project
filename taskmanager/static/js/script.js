35b0ac9e2028ca2f0398588f77a833db63ca3a74
Russ-S <rstanleym74@gmail.com>
2023-06-04 13:06:35 +0000

    create add_task.html, create route, add materialise fields

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,6 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     // sidenav initialisation
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // date picker initialisation
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+
+    // select initialisation
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
 
 // // modal initilaisation
