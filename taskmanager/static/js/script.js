2205f4fb1db40890d9914db206a265f617ef7945
Russ-S <rstanleym74@gmail.com>
2023-06-04 11:59:50 +0000

    add delete_category route and update delete button link on categories page

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,3 +3,16 @@ document.addEventListener('DOMContentLoaded', function () {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
 });
+
+// // modal initilaisation
+
+// $(document).ready(function () {
+//     $('#modal-{{ category.id }}').modal();
+// });
+
+// // Modal event listener
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var elems = document.querySelectorAll('.modal');
+//     var instances = M.Modal.init(elems, options);
+// });
