01d31e885f14798524ba4ba19414a1425156daf8
Russ-S <rstanleym74@gmail.com>
2023-06-05 04:49:50 +0000

    add edit_task route

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -58,3 +58,24 @@ def add_task():
         db.session.commit()
         return redirect(url_for("home"))
     return render_template("add_task.html", categories=categories)
+
+
+@app.route("/edit_task/<int:task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    if request.method == "POST":
+        task.task_name = request.form.get("task_name")
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("edit_task.html", task=task)
+
+
+# @app.route("/edit_task/<int:task_id>", methods=["GET", "POST"])
+# def edit_task(task_id):
+#     task = Task.query.get_or_404(task_id)
+#     if request.method == "POST":
+#         task.task_description=request.form.get("task_description"),
+#         db.session.commit()
+#         return redirect(url_for("home"))
+#     return render_template("edit_task.html", task=task)
+
