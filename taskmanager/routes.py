ad2791507d367944a50f8ce1af81c76b2f69c16b
Russ-S <rstanleym74@gmail.com>
2023-06-05 11:34:04 +0000

    create route for delete_task and add delete button to tasks.html page.  Copied form delete category code

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -75,6 +75,14 @@ def edit_task(task_id):
     return render_template("edit_task.html", task=task, categories=categories)
 
 
+@app.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
 # @app.route("/edit_task/<int:task_id>", methods=["GET", "POST"])
 # def edit_task(task_id):
 #     task = Task.query.get_or_404(task_id)
